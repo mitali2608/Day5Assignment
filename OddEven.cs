16c0455ad8ff8faea979c1f34ed1cd5bdd474d92
mitali2608 <mitalibawankule21@gmail.com>
2021-09-16 00:58:25 +0530

    OddEven program added

---
+++ OddEven.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_Assignment
+{
+    class OddEven
+    {
+        public static void GetOddEven()
+        {
+            int i;
+            Console.Write("Enter the value : ");
+            i = Convert.ToInt32(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.Write("Given Value is Even Number");
+            }
+            else
+            {
+                Console.Write("Given value is Odd Number");
+            }
+        }
+    }
+}
