31dfe77440f0d4c476cc0dc0f924bcaac867e7b8
mitali2608 <mitalibawankule21@gmail.com>
2021-09-16 00:51:49 +0530

    Factors program added

---
+++ Factors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_Assignment
+{
+    class Factors
+    {
+      public static void GetFactors()
+        {
+            int Number;
+            int x;
+
+            Console.WriteLine("Enter the value");
+            Number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" The factors of " + Number + " are- ");
+
+            for (x = 1; x <= Number; x++)
+            {
+                if (Number % x == 0)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+        }
+    }
+}
