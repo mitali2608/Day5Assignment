1ddab03eeb38e3f7f4a88758cd9d31b9b948edc7
mitali2608 <mitalibawankule21@gmail.com>
2021-09-16 00:59:31 +0530

    Powerof2 program added

---
+++ Powerof2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_Assignment
+{
+
+    class Powerof2
+    {
+        public static void GetPowerof2()
+        {
+            int N;
+            int result;
+            int counter = 1;
+
+            Console.WriteLine("Enter the value of N");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            if (N > 31)
+                Console.WriteLine("Please enter a valid input (The value of N should lies between 0 to 31) ");
+            else
+                while (counter <= N)
+                {
+                    result = counter * 2;
+                    counter++;
+                    Console.WriteLine(result);
+                }
+
+        }
+
+    }
+}
