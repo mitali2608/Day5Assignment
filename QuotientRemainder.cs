4c851b6a6859e045313d53a1c3b8d490bc6d1fd7
mitali2608 <mitalibawankule21@gmail.com>
2021-09-16 01:00:04 +0530

    QuotientRemainder program added

---
+++ QuotientRemainder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_Assignment
+{
+    class QuotientRemainder
+    {
+        public static void GetQR()
+        {
+            int Dividend;
+            int Divisor;
+
+            Console.WriteLine("Enter the value for Dividend");
+            Dividend = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value for Divisor");
+            Divisor = Convert.ToInt32(Console.ReadLine());
+
+            int Quotient = Dividend / Divisor;
+            int Remainder = Dividend % Divisor;
+
+
+            Console.WriteLine("Quotient = " + Quotient);
+            Console.WriteLine("Remainder = " + Remainder);
+
+        }
+    }
+}
