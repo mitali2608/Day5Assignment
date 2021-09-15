aebdc6a4598b4a2dd37172891c83a1d0df50173c
mitali2608 <mitalibawankule21@gmail.com>
2021-09-16 00:53:20 +0530

    harmonics program added

---
+++ Harmonic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_Assignment
+{
+    class Harmonic
+    {
+        public static void GetNthValue()
+        {
+            int N;
+            float harmonic = 1;
+
+            Console.WriteLine("Enter nth harmonic number");
+            N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= N; i++)
+            {
+                harmonic += (float)1 / i;
+            }
+            Console.WriteLine(harmonic);
+
+        }
+    }
+}
