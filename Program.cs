4e9ca7fdd95faaa7d58d8832985719ab409f8d4f
mitali2608 <mitalibawankule21@gmail.com>
2021-09-16 00:47:31 +0530

    Added Main program

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day5_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LeapYear.GetLeapYear();
+            //Powerof2.GetPowerof2();
+            //Harmonic.GetNthValue();
+            //Factors.GetFactors();
+            //QuotientRemainder.GetQR();
+            //SwapNumber.GetSwappedNumber();
+            //OddEven.GetOddEven();
+            //Alphabet.GetResult();
+            //LargeNumber.GetLargenumber();
+            CoinFlip.GetCoinFlip();
+
+            Console.ReadKey();
+        }
+    }
+}
